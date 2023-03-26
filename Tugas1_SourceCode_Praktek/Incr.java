3d965fa6c7ccd2b7258f2f7849ec27cf2540279b
Fadiyahsalsa20 <127600382+Fadiyahsalsa20@users.noreply.github.com>
2023-03-26 00:01:22 -1200

    Add files via upload

---
+++ Tugas1_SourceCode_Praktek/Incr.java
@@ -0,0 +1,19 @@
+/* Fadiyah Salsabila Abdullah
+ * 13020210203
+ * B1
+ */
+
+package com.mycompany.incr;
+
+/* Effek dari operator ++ */
+ public class Incr {
+    public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    /* Kamus */
+     int i, j;
+    /* Program */
+      i = 3;
+      j = i++;
+      System.out.println ("Nilai i : " + (++i) + "\nNilai j : " + j);
+      }
+ }
---
+++ Tugas1_SourceCode_Praktek/PrintHello.java
@@ -0,0 +1,21 @@
+/* Fadiyah Salsabila Abdullah
+ * 13020210203
+ * B1
+ */
+
+
+public class PrintHello {
+    /**
+    * @param args
+    */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* menuliskan hello ke layar */
+        System.out.print("Hello");
+        /* menuliskan hello dan ganti baris*/
+        System.out.print("\nHello ");
+        /* menuliskan hello dan ganti baris*/
+        System.out.println("World");
+        System.out.println("Welcome");
+}
+}
