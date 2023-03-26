4d7aa2b1a3ea3b72b5bf534ac4f3a2cead6d70d1
Fadiyahsalsa20 <127600382+Fadiyahsalsa20@users.noreply.github.com>
2023-03-25 23:41:39 -1200

    Add files via upload

---
+++ Asgdll.java
@@ -0,0 +1,18 @@
+/* Fadiyah Salsabila Abdullah
+ * 13020210203
+ * B1
+ */
+
+package com.mycompany.asgdll;
+public class Asgdll {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        /* Kamus */
+        float f= 20.0f;
+        double f11;
+        
+        /* Algoritma */
+        f11=10.0f;
+        System.out.println("f : " +f+ "\nf11: "+f11);
+    }
+}
